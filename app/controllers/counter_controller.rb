1b15ae469438f956681ab84bda320959ed1828d6
Philip Kallberg <philip@cloud66.com>
2013-08-26 16:00:22 +0100

    update

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -2,6 +2,7 @@ class CounterController < ApplicationController
 	def counter
 		# CPU load variable
 
+		i = 1
 		while i < 100000
 			data = IO.readlines("/var/deploy/pk_load-demo/web_head/current/lib/tasks/cpu.txt")
 			data.each do |line|
