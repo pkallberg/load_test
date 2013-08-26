56d68622d4c488c14aaadbd2e2587f8396d77370
Philip Kallberg <philip@cloud66.com>
2013-08-26 14:11:03 +0100

    update

--- app/controllers/counter_controller.rb
+++ app/controllers/counter_controller.rb
@@ -1,11 +1,8 @@
 class CounterController < ApplicationController
 	def counter
-		#data = IO.readlines("/lib/tasks/log.txt")
-		#data.each do |line|
-		# 	@cpu = line
-		#end
-
-
-			@my_vars = rand(100)
+		data = IO.readlines("/var/deploy/pk_load-demo/web_head/current/lib/tasks/log.txt")
+		data.each do |line|
+		 	@cpu = line
+		end
 end
 end
--- lib/tasks/cpu.sh
+++ lib/tasks/cpu.sh
@@ -18,7 +18,7 @@ while true; do
   let "DIFF_IDLE=$IDLE-$PREV_IDLE"
   let "DIFF_TOTAL=$TOTAL-$PREV_TOTAL"
   let "DIFF_USAGE=(1000*($DIFF_TOTAL-$DIFF_IDLE)/$DIFF_TOTAL+5)/10"
-  echo -en "$DIFF_USAGE" > log.txt
+  echo -en "$DIFF_USAGE" > cpu.txt
 
   # Remember the total and idle CPU times for the next check.
   PREV_TOTAL="$TOTAL"
