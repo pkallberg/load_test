a686fa4b38f42a0e1ce27982a4cce0b987182dc7
Philip Källberg <philip@cloud66.com>
2013-10-04 13:08:10 +0100

    Update cpu_controller.rb

--- app/controllers/cpu_controller.rb
+++ app/controllers/cpu_controller.rb
@@ -2,9 +2,9 @@ class CpuController < ApplicationController
 	respond_to :json, :html
 	def index
 		cpu = File.read("lib/tasks/cpu.txt")
-		respond_with do |index|
-			index.html { render :json => { :value => cpu } }
-			index.json { render :json => { :value => cpu } }
+		respond_with do |format|
+			format.html { render :json => { :value => cpu } }
+			format.json { render :json => { :value => cpu } }
 		end
 	end
 end
